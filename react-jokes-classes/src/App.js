98caf7e1192f429fd9086bc6d4ad0c8f8357aaaa
Alex Erhardt <90980904+SearchForSunnyD@users.noreply.github.com>
2024-06-11 17:47:28 -0500

    Add files via upload

---
+++ react-jokes-classes/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import JokeList from "./JokeList";
+
+/** App component. Renders list of jokes. */
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <JokeList />
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+export default function App() {
+  return (
+    <div className="App">
+      <JokeList />
+    </div>
+  );
+}
